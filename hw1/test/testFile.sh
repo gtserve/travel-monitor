5ea3f7c8f9f4228861869d854b9468c5a4886394
George Tservenis <george.tservenis@gmail.com>
2021-04-08 20:01:39 +0300

    [HW1] testFile.sh first implementation

--- hw1/test/testFile.sh
+++ hw1/test/testFile.sh
@@ -8,17 +8,23 @@
 # date:    25-03-2021
 # -----------------------------------------------------------------------------
 
-# Error codes
 ERR_NUM_ARGS=1
 ERR_FILE_IO=2
 #ERR_WRONG_ARG=3
 
-# Print the proper usage of the script.
+NAMES_FILE="names.txt"
+SURNAMES_FILE="surnames.txt"
+
+MAX_ID=10000
+
+# ------------------------------ Functions ------------------------------------
 print_usage() {
+    # Print the proper usage of the script.
     echo "Script usage:"
     echo "  bash ./testFile.sh [virusesFile] [countriesFile]" \
-         "[numLines] [duplicatesAllowed]"
+        "[numLines] [duplicatesAllowed]"
 }
+# -----------------------------------------------------------------------------
 
 # Check the number of arguments.
 if [[ "$#" -ne 4 ]]; then
@@ -47,48 +53,106 @@ if [[ ! -r "$2" ]]; then
     exit "$ERR_FILE_IO"
 fi
 
-v_file="$1"
-c_file="$2"
-num_lines="$3"
+viruses_file="$1"
+countries_file="$2"
+num_records="$3"
 dupl_flag="$4"
 
 # Print a useful message.
-message="Creating $num_lines records with"
-if [[ "$dupl_flag" = 0 ]]; then
+message="Creating $num_records records with"
+if [[ "$dupl_flag" -eq 0 ]]; then
     message="$message no"
 fi
-echo "$message duplicates, using '$v_file' and '$c_file'..."
+echo "$message duplicates, using '$viruses_file' and '$countries_file'..."
 
+# Save files to arrays for faster access.
+num_names=0
+declare -a name_array
+while read -r line; do
+    name_array+=("$line")
+    num_names=$((num_names + 1))
+done <"$NAMES_FILE"
 
-# Count viruses and countries
-num_viruses=$(wc -l < "$v_file")
-num_countries=$(wc -l < "$c_file")
-echo "Read $num_viruses viruses and $num_countries countries."
+num_surnames=0
+declare -a surname_array
+while read -r line; do
+    surname_array+=("$line")
+    num_surnames=$((num_surnames + 1))
+done <"$SURNAMES_FILE"
+
+num_countries=0
+declare -a country_array
+while read -r line; do
+    country_array+=("$line")
+    num_countries=$((num_countries + 1))
+done <"$countries_file"
+
+num_viruses=0
+declare -a virus_array
+while read -r line; do
+    virus_array+=("$line")
+    num_viruses=$((num_viruses + 1))
+done <"$viruses_file"
+
+echo "Using $num_viruses viruses, $num_countries countries," \
+    "$num_names names and $num_surnames surnames."
 
 # Create new empty input file.
 i_file="input.txt"
-if [[ -e "$i_file" ]]; then
-    rm -f "$i_file"
-fi
+if [[ -e "$i_file" ]]; then rm -f "$i_file"; fi
 touch "$i_file"
 
+if [[ "$dupl_flag" -eq 0 ]];
+    then
+    id_array=($(seq "$MAX_ID" | shuf))
+    else
+    num_duplicates=$((1 + (num_records / 10) + (RANDOM % (num_records / 10))))
+    id_array=($(seq "$MAX_ID" | shuf | head -n $((num_records - num_duplicates))))
+    id_array+=($(printf '%s\n' "${id_array[@]}" | shuf | head -n "$num_duplicates"))
+    id_array=($(printf '%s\n' "${id_array[@]}" | shuf ))
+fi
 
 index=0
-declare -a id_hash_map      # For unique IDs
-declare -a names_hash_map   # For unique names
-while [[ "$index" -lt "$num_lines" ]]; do
+while [[ "$index" -lt "$num_records" ]]; do
 
-    citizen_id=$((RANDOM % 9999))
+    # Get a random citizen_id between [0 - MAX_ID]
+    citizen_id="${id_array["$index"]}"
 
-    while [[ "${id_hash_map["$citizen_id"]}" -eq 1 ]]; do
-        citizen_id=$((RANDOM % 10))
-    done
+    # Get a random first name from array.
+    i=$((RANDOM % num_names))
+    name="${name_array["$i"]}"
 
-    id_hash_map["$citizen_id"]=1
+    # Get a random surname from array.
+    i=$((RANDOM % num_surnames))
+    surname="${surname_array["$i"]}"
 
-    echo "$citizen_id" >> input.txt
-    index=$((index+1))
+    # Get a random country from array.
+    i=$((RANDOM % num_countries))
+    country="${country_array["$i"]}"
+
+    # Get random age between [0 - 120]
+    age=$((RANDOM % 121))
+
+    # Get a random virus from array.
+    i=$((RANDOM % num_viruses))
+    virus="${virus_array["$i"]}"
+
+    record="$citizen_id $name $surname $country $age $virus"
+
+    # Random YES/NO for vaccination.
+    vaccinated=$((RANDOM % 2))
+    if [[ "$vaccinated" -eq 1 ]]; then
+        # Get a random date.
+        year=$((1796 + (RANDOM % 225))) # 1796 - 2021
+        month=$((1 + (RANDOM % 12)))    # 1 - 12
+        day=$((1 + (RANDOM % 30)))      # 1 - 30
+        record="$record YES $day-$month-$year"
+    else
+        record="$record NO"
+    fi
+
+    echo "$record" >>input.txt
+    index=$((index + 1))
 done
-
 
 exit 0
