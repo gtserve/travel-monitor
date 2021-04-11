dc130a60dc76d185e8cedf6f44619b8b4d7c1c6f
George Tservenis <george.tservenis@gmail.com>
2021-04-11 23:48:56 +0300

    [HW1] Change date and update README

--- hw1/include/commands.h
+++ hw1/include/commands.h
@@ -3,7 +3,7 @@
  * System Programming - Homework 1
  * name:    George Tservenis
  * email:   sdi1500164@di.uoa.gr
- * date:    25-03-2021
+ * date:    11-04-2021
  * ----------------------------------------------------------------------------
  */
 
