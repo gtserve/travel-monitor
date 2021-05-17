9a6b9443acbcf759afeda2458128c3968057fb9d
George Tservenis <george.tservenis@gmail.com>
2021-05-17 16:13:05 +0300

    Add HW2

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 UoA, K24 System Programming - Spring Semester 2021
 
 - Homework 1: Vaccine Monitor
-- Homework 2: 
+- Homework 2: Travel Monitor
 - Homework 3: 
---
+++ hw2/src/travelmonitor.c
@@ -0,0 +1,15 @@
+/* travelmonitor.c
+* -------------------------------------------------------------------------------------------------
+* course:   System Programming
+* project:  (#2) Travel Monitor
+* name:     George Tservenis
+* email:    sdi1500164@di.uoa.gr
+* -------------------------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World!\n");
+    return 0;
+}
